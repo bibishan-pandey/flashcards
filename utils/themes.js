6ec6f26aeda3c13753adecb0fed5f7ea66d95079
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-14 12:57:15 +0545

    feat: themes utils added

---
+++ utils/themes.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "react-native-paper";
+import { error, primary, secondary } from "./colors";
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: primary,
+    secondary: secondary,
+    error: error,
+  },
+};
